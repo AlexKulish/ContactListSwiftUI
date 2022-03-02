949d2a683589d682b80bf2e562210564109aed64
Alexey Kulish <alexkulish1993@gmail.com>
2022-03-02 13:44:48 +0300

    Add model Person

---
+++ ContactListSwiftUI/Person.swift
@@ -0,0 +1,43 @@
+//
+//  Person.swift
+//  ContactListSwiftUI
+//
+//  Created by Alex Kulish on 02.03.2022.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phone: String
+    
+    var fullname: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    
+    static func getPersons() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let emails = DataManager.shared.surnames.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        for index in 0..<names.count {
+            let person = Person(name: names[index],
+                                surname: surnames[index],
+                                email: emails[index],
+                                phone: phones[index])
+            
+            persons.append(person)
+        }
+        return persons
+    }
+    
+}
